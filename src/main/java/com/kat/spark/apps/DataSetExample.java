0dff28e7f45a8d7872320fb5ffefa80d3ce8e46a
gitKatia <katia.iacomussi@libero.it>
2022-05-15 21:24:22 +0100

    Add map and reduce transformations

--- src/main/java/com/kat/spark/apps/DataSetExample.java
+++ src/main/java/com/kat/spark/apps/DataSetExample.java
@@ -1,6 +1,8 @@
 package com.kat.spark.apps;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.api.java.function.ReduceFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
@@ -28,10 +30,17 @@ public class DataSetExample {
 
 		// I can get a data set from a list
 		Dataset<String> wordDataSet =  sparkSession.createDataset(words, Encoders.STRING());
+		applyFilterTransformation(wordDataSet);
+		applyMapTransformation(wordDataSet);
+		applyReduceTransformation(wordDataSet);
+	}
+
+	private static void applyFilterTransformation(Dataset<String> wordDataSet) {
 		// I can get a data frame from a data set
 		Dataset<Row> wordDataFrame =  wordDataSet.toDF();
+		log.info("Show dataframe:");
 		wordDataFrame.show();
-		
+
 		List<String> filterList = Arrays.asList("'this'", "'is'", "'he'");
 
 		String filters = filterList.stream()
@@ -39,10 +48,17 @@ public class DataSetExample {
 				.collect(Collectors.joining(",", "(", ")"));
 		String sqlFilter = format("value not in %s", filters);
 		Dataset<Row> filteredDf = wordDataFrame.filter(sqlFilter);
-
+		log.info("Show filtered dataframe:");
 		filteredDf.show();
-
-		
 	}
 
+	private static void applyMapTransformation(Dataset<String> wordDataSet) {
+		Dataset<String> dataSet = wordDataSet.map((MapFunction<String, String>) row -> "word: " + row, Encoders.STRING());
+		dataSet.show(10);
+	}
+
+	private static void applyReduceTransformation(Dataset<String> wordDataSet) {
+		String reducedValue = wordDataSet.reduce((ReduceFunction<String>) (v1, v2) -> v1 + "-" + v2);
+		log.info(reducedValue);
+	}
 }
